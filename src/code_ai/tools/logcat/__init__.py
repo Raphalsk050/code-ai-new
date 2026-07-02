820fb2abb12aa3e3c81ee496ba78030681a3afa3
Rafael Morais <ram.morais50@gmail.com>
2026-07-02 07:20:07 -0400

    Wire analyze_logcat tool with adb, file, and inline sources

---
+++ src/code_ai/tools/logcat/__init__.py
@@ -0,0 +1,5 @@
+from code_ai.tools.logcat.analyze_logcat import AnalyzeLogcatTool
+from code_ai.tools.logcat.analyzer import LogcatAnalyzer
+from code_ai.tools.logcat.parser import LogcatParser
+
+__all__ = ["AnalyzeLogcatTool", "LogcatAnalyzer", "LogcatParser"]
